01cf3f9fc7b68097a948abba2d8bd4f1dbcc491e
Wįłłý Fœx <BlackVikingPro@users.noreply.github.com>
2017-11-06 15:20:07 -0600

    initial commit

---
+++ arescrypt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arescrypt
+{
+    class Program
+    {
+        static string sessionUsername = System.Environment.UserName;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, " + sessionUsername);
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey(); // Hang the console
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Arescrypt - (init) v0.001
+### -- Experimental ransomware using Bitcoin/Litecoin/Zcash
+
+***
